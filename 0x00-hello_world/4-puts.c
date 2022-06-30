06096532b0b09ce029ab53b3937af0131a003e62
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-06-30 19:10:25 +0300

    0x00-hello_world 4 betty check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/**
- * Print using puts() function
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
