387aec5a0b8573eeba6347c027af12f90bb1d509
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-06 22:37:47 +0300

    0x02-functions_nested_loops 102 and 103 done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Fibonacci Numbers
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	long int f1, f2, f3;
+
+	f1 = 1;
+	f2 = 2;
+	printf("%ld", f1);
+	printf(", %ld", f2);
+	for (a = 0; a < 48; a++)
+	{
+		f3 = f1 + f2;
+		printf(", %ld", f3);
+		f1 = f2;
+		f2 = f3;
+	}
+	printf("\n");
+	return (0);
+}
