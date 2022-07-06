c598f781ae34bea7b657f2d93182ec4a762232e8
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-06 16:23:20 +0300

    0x02-functions_nested_loops 4 done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks is param is a letter.
+ * @c: The character to eval
+ * Return: Success(1), Failure(0)
+ */
+int _isalpha(int c)
+{
+	if ( (c >= 65 && c <= 90) || (c >= 97 && c <= 112) )
+		return (1);
+	else
+		return (0);
+
+}
