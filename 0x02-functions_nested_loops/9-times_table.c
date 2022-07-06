d8a5888599c0118c37a014d72bd071d37cc445a7
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-06 21:25:38 +0300

    0x02-functions_nested_loops 9 done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the hours and minutes in a day.
+ * Return: does not return
+ */
+void times_table(void)
+{
+	int r, c, t;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			t = r * c;
+			if (t > 9)
+				_putchar('0' + (t / 10));
+			_putchar('0' + (t % 10));
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			} else
+			{
+				_putchar('$');
+			}
+			_putchar('\n');
+		}
+
+	}
+
+}
