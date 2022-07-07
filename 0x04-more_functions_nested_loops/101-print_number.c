a660960443ad11cc8c7235d733a9791d69cf2ea5
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-07 23:01:16 +0300

    0x04-more_functions_nested_loops bonus questions

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: none.
+ */
+void print_number(int n)
+{
+	unsigned int m, d;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = 0;
+	while (m > 0)
+	{
+		d *= 10;
+		d += m % 10;
+		m /= 10;
+	}
+
+	while (d > 0)
+	{
+		_putchar('0' + (d % 10));
+		d /= 10;
+	}
+}
