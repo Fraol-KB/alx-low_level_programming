b7fa64097f143065e9ff01902a99846986ff9779
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-11 20:40:28 +0300

    0x05-pointers_arrays_strings 6 fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts2 - Prints String
+ * puts2 - Prints String
  * @str: string pointer
  * Return: none.
  */
-void _puts2(char *str)
+void puts2(char *str)
 {
 	int a;
 
