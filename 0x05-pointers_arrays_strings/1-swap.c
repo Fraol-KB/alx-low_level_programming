d139be8e2c57219e643a39403e6cfadbbecd1463
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-11 17:03:21 +0300

    0x05-pointers_arrays_strings 0-1 done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps values of two integers
+ * @a: pointer of var 1.
+ * @b: pointer of var 2.
+ * Return: none.
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
