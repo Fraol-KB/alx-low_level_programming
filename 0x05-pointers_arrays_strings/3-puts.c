9c780ce7d9dfd29b44aaa70b0d24e365da5fcc8b
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-11 19:13:53 +0300

    0x05-pointers_arrays_strings 4 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Prints String
+ * @str: string pointer
+ * Return: none.
+ */
+void _puts(char *str)
+{
+	int a;
+
+	a = 0;
+	while (*(s + a))
+	{
+		_putchar(*(s + a));
+		a++;
+	}
+	_putchar('\n');
+}
