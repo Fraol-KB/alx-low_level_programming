576db356e5507f1c5290f04da033496e883bf78f
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-12 18:23:27 +0300

    0x05-pointers_arrays_strings bonus questions 10 done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi- convert string to int.
+ * @s: string to convert
+ * Return: converted int.
+ */
+int _atoi(char *s)
+{
+	int i, r;
+
+	i = 0;
+	r = 0;
+	while (*(src + i))
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			r = r * 10;
+			if (*(s + i - 1) == '-' && r == 0)
+				r = -1;
+			else if (r == 0)
+				r = 1;
+
+			r += (*(s + i) - '0');
+
+			if (*(s + i + 1) > '9' || *(s + i + 1) < '0')
+			{
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (r);
+}
