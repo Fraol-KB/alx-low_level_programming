68e24e888078be1fb9b618a14934691a452d0e17
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-14 20:06:53 +0300

    0x06-pointers_arrays_strings Tasks 2 done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  *_strncpy- copy src to dest.
+ * @dest: dest of string
+ * @src: src of string
+ * @n: no of char to copy.
+ * Return: none.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i) && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
