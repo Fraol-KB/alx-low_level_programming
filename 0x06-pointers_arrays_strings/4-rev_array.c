fd02861e1e4ff9841542902c76842990d8ef8a45
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-15 19:26:19 +0300

    0x06-pointers_arrays_strings Tasks 4-5

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - reverses array
+ * @a: given array.
+ * @n: number of elements of the array.
+ * Return: no return.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, swap;
+
+	i = 0;
+	while (i < (n - 1))
+	{
+		j = i + 1;
+		while (j > 0)
+		{
+			swap = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = swap;
+			j--;
+		}
+	}
+}
