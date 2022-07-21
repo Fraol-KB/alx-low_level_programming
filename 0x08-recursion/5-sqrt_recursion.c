e61e3fe62ad0246adcd0140021bad893e292fa85
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-21 23:18:51 +0300

    0x08-recursion tasks 5 fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- * Return: natural square root.
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	return (sqrt_finder(n, 2));
-}
-
-/**
  * sqrt_finder -finds the square that results n.
  * @n: number.
  * @c: potential square root of n.
@@ -33,4 +17,21 @@ int sqrt_finder(int n, int c)
 		return (-1);
 	}
 	return (sqrt_finder(n, c + 1));
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (sqrt_finder(n, 2));
 }
