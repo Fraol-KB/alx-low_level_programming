966394afc078537e9befc2318823559f2685afee
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-22 23:23:06 +0300

    0x0A-argc_argv Tasks tasks 0-1 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the the name of the file name
+ * @argc: number of arguments
+ * @argv: array of argurment.
+ * Return: number of args.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments given
+ * @argc: number of arguments
+ * @argv: array of argurment.
+ * Return: number of args.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", --argc);
+}
