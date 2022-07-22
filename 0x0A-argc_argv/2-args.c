0ecb63583bd0a284c30fa096b01f58c17122d33e
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-22 23:45:38 +0300

    0x0A-argc_argv Tasks tasks 2 done .

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the arguments provided
+ * @argc: number of arguments
+ * @argv: array of argurment.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
