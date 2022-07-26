0fb60400bbbeb8bbba2175019ba7e0d1cb93b4db
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-07-26 20:14:32 +0300

    0x0B-malloc_free tasks 0 fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	if (size <= 0)
 		return ('\0');
 	s = malloc(sizeof(char) * size);
+	if (!s)
+		return ('\0');
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
